2e2ec7a0a27dfc43687f2e370221f319ff5f43b9
Bruno Martins <brunocmartins@live.com.pt>
2020-01-20 17:36:46 +0000

    more things

--- original/demo1/src/main/java/org/sonar/samples/java/checks/MyCustomCheck.java
+++ original/demo1/src/main/java/org/sonar/samples/java/checks/MyCustomCheck.java
@@ -6,12 +6,29 @@ import org.sonar.plugins.java.api.tree.Tree.Kind;
 import java.util.Collections;
 import java.util.List;
 
-@Rule(key = "MyFirstCustomRule")
-public class MyCustomCheck extends IssuableSubscriberVisitor{
+@Rule(
+        key = "MyCustomCheck",
+        name = "Return type and parameter of a method should not be the same",
+        description = "For a method having a single parameter, the types of its return value and its parameter should never be the same.",
+        priority = Priority.CRITICAL,
+        tags = {"bug"})
+public class MyFirstCustomCheck extends IssuableSubscriptionVisitor {
     @Override
     public List<Kind> nodesToVisit() {
         return Collections.emptyList();
     }
+
+    @Override
+    public void visitNode(Tree tree) {
+        MethodTree method = (MethodTree) tree;
+        if (method.parameters().size() == 1) {
+            MethodSymbol symbol = method.symbol();
+            Type firstParameterType = symbol.parameterTypes().get(0);
+            Type returnType = symbol.returnType().type();
+            if (returnType.is(firstParameterType.fullyQualifiedName())) {
+                reportIssue(method.simpleName(), "Never do that!");
+            }
+        }
 
 
 }
